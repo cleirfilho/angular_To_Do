f035db23610f7032d78641039d5eed0208866f2c
cleir castro e costa filho <cleirfilho@hotmail.com.br>
2022-07-27 16:26:00 -0500

    finalizando to-do list

--- src/app/models/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/models/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
-  styleUrls: ['./todo-input-add-itens.component.scss']
+  styleUrls: ['./todo-input-add-itens.component.scss'],
 })
 export class TodoInputAddItensComponent implements OnInit {
+  @Output() emmiterTask = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  btnAddItem: string = '';
+
+  salvaItem(): void {
+    this.btnAddItem = this.btnAddItem.trim();
+    if (this.btnAddItem) this.emmiterTask.emit(this.btnAddItem);
+    this.btnAddItem = '';
   }
 
+  ngOnInit(): void {}
 }
--- src/app/models/home/components/todo-list/todo-list.component.ts
+++ src/app/models/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 import { IComponentsList } from '../i-components-list';
 
@@ -8,30 +8,55 @@ import { IComponentsList } from '../i-components-list';
   styleUrls: ['./todo-list.component.scss']
 })
 
-export class TodoListComponent implements OnInit, IComponentsList<TaskList> {
+export class TodoListComponent implements OnInit, IComponentsList<TaskList>, DoCheck {
 
 
   constructor() { }
+ 
 
-  TaskList: TaskList[] = [
-    {task: 'primeiro' , checked:true},
-    {task: 'segundo', checked:false}
-  ]
+  TaskList: TaskList[] = JSON.parse(localStorage.getItem('TaskList') || '[]')
 
   get(termoBusca?: string): void {
-    throw new Error('Method not implemented.');
+    
   }
+
   delete(id: number): void {
    this.TaskList.splice(id,1);
   }
+
   deleteAll(){
     const confitm = window.confirm("Essa ação era excluir toda a sua lista! tem certeza que deseja continuar?")
     if(confitm)
       this.TaskList = [];
   }
 
+  salvar(item:string){
+    this.TaskList.push({task:item, checked:false});
+  }
+
+  validarInput(event:string, index:number){
+    if(!event.length){
+      const confirm = window.confirm("Task esta vazia, você deseja excluir?")
+      if(confirm){
+        this.delete(index);
+      }
+    }
+
+  }
+
+  setLocalStorage(){
+    if(this.TaskList){
+      this.TaskList.sort((first,last) => Number(first.checked) - Number(last.checked))
+      localStorage.setItem('TaskList', JSON.stringify(this.TaskList));
+    }
+  }
+
 
   ngOnInit(): void {
+  }
+
+  ngDoCheck(): void {
+    this.setLocalStorage()
   }
 
 }
